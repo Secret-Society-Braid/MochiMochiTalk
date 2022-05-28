f18d210809b86847e7fa97e0783d478f237230d6
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-05-28 21:35:44 +0900

    feat: add cycle method

--- app/src/main/java/MochiMochiTalk/lib/DerepoUpdatesDetector.java
+++ app/src/main/java/MochiMochiTalk/lib/DerepoUpdatesDetector.java
@@ -3,18 +3,20 @@ package MochiMochiTalk.lib;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import javax.annotation.Nullable;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import net.dv8tion.jda.api.JDA;
 
 /**
  * でれぽ更新検知クラス
@@ -26,7 +28,7 @@ public class DerepoUpdatesDetector {
     
     private static final Logger log = LoggerFactory.getLogger(DerepoUpdatesDetector.class);
     public static final String DEREPO_UPDATE_API_URI = "https://api.matsurihi.me/cgss/v1/derepo/statuses?idolId=216&maxResults=1";
-    private static final String LATEST_UPDATE_CACHE_FILE_NAME = "derepoLatest.json";
+    private static CacheData prevCache = null;
     private static final DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'+'Z");
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
@@ -34,7 +36,8 @@ public class DerepoUpdatesDetector {
         /* do nothing */
     }
 
-    public static CacheData getDerepoUpdate() throws IOException {
+    @Nullable
+    private static CacheData getDerepoUpdate() throws IOException {
         Date date = new Date();
         final String dateNow = df.format(date);
         final String uri = DEREPO_UPDATE_API_URI + "&timeMin=" + dateNow;
@@ -50,25 +53,22 @@ public class DerepoUpdatesDetector {
         }
         JsonNode response = OBJECT_MAPPER.readTree(conn.getInputStream());
 
-        if(Files.notExists(Paths.get(LATEST_UPDATE_CACHE_FILE_NAME))) {
-            CacheData latest = new CacheData(dateNow, response);
-            OBJECT_MAPPER.writeValue(Paths.get(LATEST_UPDATE_CACHE_FILE_NAME).toFile(), latest);
-            return latest;
+        if(prevCache == null) {
+            prevCache = new CacheData(dateNow, response);
+            return null;
         }
-        CacheData latestCache = OBJECT_MAPPER.readValue(Paths.get(LATEST_UPDATE_CACHE_FILE_NAME).toFile(), CacheData.class);
         try {
             final Date latestDate = df.parse(response.get("postTime").toString());
-            final Date cacheDate = df.parse(latestCache.getLatestUpdateDateString());
+            final Date cacheDate = df.parse(prevCache.getLatestUpdateDateString());
             int compared = latestDate.compareTo(cacheDate);
             switch (compared) {
                 case 0:
                     log.info("There is no updates.");
-                    return latestCache;
+                    return null;
                 case 1:
                     log.info("There are some updates.");
-                    CacheData cache = new CacheData(dateNow, response);
-                    OBJECT_MAPPER.writeValue(Paths.get(LATEST_UPDATE_CACHE_FILE_NAME).toFile(), cache);
-                    return cache;
+                    prevCache = new CacheData(dateNow, response);
+                    return prevCache;
                 default:
                     log.warn("something went wrong. The system clock might be incorrect.");
                     break;
@@ -77,6 +77,17 @@ public class DerepoUpdatesDetector {
             log.error("Failed to parse cache date: {}", e);
         }
         return null;
+    }
+
+    public static void postDataCycle(JDA jda) {
+        try {
+            final CacheData latest = getDerepoUpdate();
+            if(latest != null) {
+                // TODO: Complete implementation
+            }
+        } catch (IOException e) {
+            log.error("There was a problem while getting derepo update.", e);
+        }
     }
 
 }

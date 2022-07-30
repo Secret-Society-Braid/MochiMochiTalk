2e58502e742d46a800b05475c425828caef23238
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-07-30 18:47:49 +0900

    feat: add handling action interface

--- app/src/main/java/MochiMochiTalk/lib/comms/CommunicationAction.java
+++ app/src/main/java/MochiMochiTalk/lib/comms/CommunicationAction.java
@@ -1,5 +1,27 @@
 package MochiMochiTalk.lib.comms;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
 public interface CommunicationAction {
+
+    void queue();
+
+    void queue(boolean acceptInterruptIfneeded);
+
+    void queue(Runnable success);
+
+    void queue(Runnable success, Consumer<? super Throwable> failure);
+
+    void queue(Runnable success, Consumer<? super Throwable> failure, boolean acceptInterruptIfNeeded);
     
+    CompletableFuture<Void> submit();
+
+    CompletableFuture<Void> submit(boolean acceptInterruptIfNeeded);
+
+    CompletableFuture<Void> submit(Runnable success);
+
+    CompletableFuture<Void> submit(Runnable success, Consumer<? super Throwable> failure);
+
+    CompletableFuture<Void> submit(Runnable success, Consumer<? super Throwable> failure, boolean acceptInterruptIfNeeded);
 }

7769f427222b98fd4dfa15538e9ae113ae11136c
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-07-31 23:53:08 +0900

    chore: file added

---
+++ app/src/test/java/MochiMochiTalk/libs/CacheControllTest.java
@@ -0,0 +1,16 @@
+package MochiMochiTalk.libs;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class CacheControllTest {
+
+    @Test
+    public void test() {
+
+        assertTrue(true);
+
+    }
+    
+}

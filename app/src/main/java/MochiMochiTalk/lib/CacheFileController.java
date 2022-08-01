1659525969ca08b9a6c61e444ca3b12096207cb4
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-08-01 22:06:54 +0900

    test: add test codes

--- app/src/main/java/MochiMochiTalk/lib/CacheFileController.java
+++ app/src/main/java/MochiMochiTalk/lib/CacheFileController.java
@@ -2,27 +2,32 @@ package MochiMochiTalk.lib;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import com.google.common.base.Strings;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CacheFileController {
-    
+
     private static CacheFileController singleton;
 
     public static final String DIRECTORY_NAME = ".tmp" + File.separator + ".vocalcord";
 
     private List<Path> paths;
- 
+
     public static CacheFileController getInstance() {
-        if(singleton == null)
+        if (singleton == null)
             singleton = new CacheFileController();
         return singleton;
     }
@@ -39,6 +44,37 @@ public class CacheFileController {
             log.error("Encountered I/O error while traversing cache file directory", e);
         }
         return res;
+    }
+
+    public Path storeCache(String phrase, byte[] bytes) throws IOException {
+        if (Strings.isNullOrEmpty(phrase))
+            return Paths.get(DIRECTORY_NAME);
+        String fileName = Base64.getEncoder().encodeToString(phrase.getBytes(StandardCharsets.UTF_8));
+        Path storePath = Paths.get(DIRECTORY_NAME + File.separator + fileName);
+        return Files.write(storePath, bytes);
+    }
+
+    public Optional<byte[]> getCacheBytes(String phrase) throws IOException {
+        if(Strings.isNullOrEmpty(phrase))
+            return Optional.empty();
+        String fileName = Base64.getEncoder().encodeToString(phrase.getBytes(StandardCharsets.UTF_8));
+        Path indexPath = Paths.get(DIRECTORY_NAME + File.separator + fileName);
+        if(Files.exists(indexPath))
+            return Optional.ofNullable(Files.readAllBytes(indexPath));
+        else
+            return Optional.empty();
+    }
+
+    public List<Path> getPaths() {
+        return this.paths;
+    }
+
+    public boolean update() {
+        List<Path> updated = getListOfPaths();
+        if(this.paths.equals(updated))
+            return false;
+        this.paths = updated;
+        return true;
     }
 
 }

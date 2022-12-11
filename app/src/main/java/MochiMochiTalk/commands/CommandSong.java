e0e9991c5c8b6a9483095be5a35319e86ac7fa1c
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-12-11 22:47:28 +0900

    complete implementation

--- app/src/main/java/MochiMochiTalk/commands/CommandSong.java
+++ app/src/main/java/MochiMochiTalk/commands/CommandSong.java
@@ -31,6 +31,7 @@ public class CommandSong extends ListenerAdapter {
 
     private static final Executor concurrentExecutor = Executors.newCachedThreadPool(
         new CountingThreadFactory(() -> "MochiMochiTalk", "Song detail integration thread", true));
+    private static final String DEV_USER = "399143446939697162";
 
     @Override
     public void onSlashCommandInteraction(@Nonnull SlashCommandInteractionEvent event) {
@@ -39,8 +40,10 @@ public class CommandSong extends ListenerAdapter {
         CompletableFuture<InteractionHook> earlyReplyFuture = event.reply("検索を開始します…お待ちください。（Powered by ふじわらはじめAPI）").submit();
         final String subCommandName = Objects.requireNonNull(event.getSubcommandName());
         log.info("got interaction with following command: {} in {}", subCommandName, event.getName());
+
         if(subCommandName.equals("id")) {
             int id = event.getOption(subCommandName, () -> -1 ,OptionMapping::getAsInt);
+
             if(id == -1) {
                 InteractionHook hook = event.getHook();
                 earlyReplyFuture.thenAcceptBothAsync(
@@ -49,20 +52,75 @@ public class CommandSong extends ListenerAdapter {
                     concurrentExecutor);
                 return;
             }
+
             MusicEndPointBuilder builder = MusicEndPointBuilder.createWith(id);
             builder.setHide(
                 MusicParameter.Hide.CD_MEMBER,
                 MusicParameter.Hide.LIVE_MEMBER
             );
+
             CompletableFuture<Message> sendEmbedFuture = earlyReplyFuture.thenCombineAsync(
                 builder.build().submit(),
                 (earlyReplyInteraction, response) -> earlyReplyInteraction.editOriginalEmbeds(createSongDetailMessage(response)).complete(),
                 concurrentExecutor);
-            sendEmbedFuture.whenCompleteAsync(
-                (ret, ex) -> {
-                    // TODO: write handling code
-                }
-            );
+
+            sendEmbedFuture.handleAsync(
+                (message, t) -> {
+
+                    if(t == null) {
+                        log.debug("successfully sent song detail message to user {}", event.getUser());
+                        return 0;
+                    }
+
+                    int exitCode = -1;
+                    EmbedBuilder reportBuilder = new EmbedBuilder();
+                    reportBuilder.setTitle("Automatic error report");
+                    reportBuilder.setDescription("発生箇所：`CommandSong#onSlashCommandInteraction`");
+                    log.warn("This exception is going to be reported to the developer automatically.");
+
+                    if(t.getCause() != null) {
+                        log.warn("failed to send song detail message to user {}", event.getUser(), t.getCause());
+                        final String className = t.getCause().getClass().getName();
+                        final String exceptionMessage = t.getCause().getMessage();
+                        reportBuilder.addField("例外名", className != null ? className : "null", false);
+                        reportBuilder.addField("例外メッセージ", exceptionMessage != null ? exceptionMessage : "null", false);
+
+                        exitCode = 1;
+
+                    } else {
+                        log.warn("There was an exception while handling slash command.", t);
+                        final String className = t.getClass().getName();
+                        final String exceptionMessage = t.getMessage();
+                        reportBuilder.addField("例外名", className != null ? className : "null" , false);
+                        reportBuilder.addField("例外メッセージ", exceptionMessage != null ? exceptionMessage : "null", false);
+                    }
+
+                    message.getJDA().openPrivateChannelById(DEV_USER)
+                        .submit()
+                        .thenAcceptAsync(
+                            privateChannel -> privateChannel.sendMessageEmbeds(reportBuilder.build()).submit(),
+                            concurrentExecutor);
+
+                    return exitCode;
+                },
+            concurrentExecutor).whenCompleteAsync(
+                (result, t) -> {
+                    if(t == null) {
+                        log.debug("successfully sent error report to developer");
+                        return;
+                    }
+                    switch (result) {
+                        case 1:
+                            log.warn("failed to send error report to developer", t);
+                            break;
+                        case -1:
+                            log.error("There was an unexpected exception while handling slash command.", t);
+                            break;
+                        default:
+                            throw new IllegalStateException("Unexpected completion status: must not happen with : " + result);
+                    }
+                },
+            concurrentExecutor);
         }
     }
 

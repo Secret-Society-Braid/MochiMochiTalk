ea699e234e651b735306143f04250c48fabb97a5
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-12-22 17:01:09 +0900

    fix: organize unused imports

--- app/src/main/java/MochiMochiTalk/App.java
+++ app/src/main/java/MochiMochiTalk/App.java
@@ -3,10 +3,6 @@
  */
 package MochiMochiTalk;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,13 +17,11 @@ import MochiMochiTalk.commands.CommandShutdown;
 import MochiMochiTalk.commands.CommandSong;
 import MochiMochiTalk.commands.CommandWhatsNew;
 import MochiMochiTalk.commands.SlashCommandRegisteration;
-import MochiMochiTalk.lib.DerepoUpdatesDetector;
 import MochiMochiTalk.lib.FileReadThreadImpl;
 import MochiMochiTalk.listeners.CheckContainsDiscordURL;
 import MochiMochiTalk.listeners.EventLogger;
 import MochiMochiTalk.listeners.ReadyListener;
 import MochiMochiTalk.voice.nvoice.EventListenerForTTS;
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;

fdc3d2ae5949ab6b9de4a0994c966fff3e1f9a7d
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-03-03 12:13:55 +0900

    add concurrent util class

---
+++ app/src/main/java/MochiMochiTalk/util/ConcurrencyUtil.java
@@ -0,0 +1,25 @@
+package MochiMochiTalk.util;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.function.Supplier;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.internal.utils.concurrent.CountingThreadFactory;
+
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE) // implicit private constructor
+public class ConcurrencyUtil {
+
+  public static ThreadFactory createThreadFactory(Supplier<String> threadNameSupplier) {
+    return new CountingThreadFactory(() -> "MochiMochiTalk", threadNameSupplier.get());
+  }
+
+  public static <T> void postEventHandling(T ret, Throwable t) {
+    if (t == null) {
+      log.info("Event handling completed successfully. ret={}", ret);
+    } else {
+      log.warn("There was an error while handling an event. ret={}, t={}", ret, t);
+    }
+  }
+}

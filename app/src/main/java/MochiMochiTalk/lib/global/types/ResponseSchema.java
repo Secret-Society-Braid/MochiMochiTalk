96edccbf30f9adcedebbcd16615ce93afa138b9c
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-03-19 18:44:30 +0900

    add some useful methods in ResponseSchema class

--- app/src/main/java/MochiMochiTalk/lib/global/types/ResponseSchema.java
+++ app/src/main/java/MochiMochiTalk/lib/global/types/ResponseSchema.java
@@ -1,5 +1,7 @@
 package MochiMochiTalk.lib.global.types;
 
+import MochiMochiTalk.lib.global.InvokeMethod;
+import java.util.Objects;
 import lombok.Data;
 
 @Data
@@ -9,9 +11,24 @@ public class ResponseSchema {
   private boolean exist;
   private boolean update;
 
+  private static ResponseSchema EMPTY;
+
   public ResponseSchema(String invokeMethod, boolean exist, boolean update) {
     this.invokeMethod = invokeMethod;
     this.exist = exist;
     this.update = update;
+  }
+
+  public ResponseSchema(InvokeMethod invokeMethod, boolean exist, boolean update) {
+    this.invokeMethod = invokeMethod.toString();
+    this.exist = exist;
+    this.update = update;
+  }
+
+  public static ResponseSchema createEmpty() {
+    if (Objects.isNull(EMPTY)) {
+      EMPTY = new ResponseSchema("unknown", false, false);
+    }
+    return EMPTY;
   }
 }

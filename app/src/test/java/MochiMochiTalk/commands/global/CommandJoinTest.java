9eba074cbd9e21869893392178404a929ceb3a1a
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-03-23 22:19:49 +0900

    Delete prototype mock test

--- app/src/test/java/MochiMochiTalk/commands/global/CommandJoinTest.java
+++ app/src/test/java/MochiMochiTalk/commands/global/CommandJoinTest.java
@@ -11,40 +11,12 @@ import java.util.concurrent.CompletableFuture;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.InteractionHook;
-import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class CommandJoinTest {
-
-  @Test
-  @Disabled
-  void onSlashCommandInteractionTest() {
-    SlashCommandInteractionEvent mockEvent = mock(SlashCommandInteractionEvent.class);
-    ReplyCallbackAction mockReplyCallbackAction = mock(ReplyCallbackAction.class,
-        RETURNS_DEEP_STUBS);
-    InteractionHook mockHook = mock(InteractionHook.class, RETURNS_DEEP_STUBS);
-
-    when(mockEvent.reply(anyString())).thenReturn(mockReplyCallbackAction);
-    when(mockReplyCallbackAction.setEphemeral(true).submit()).thenReturn(
-        CompletableFuture.completedFuture(mockHook));
-    when(mockHook.editOriginal(anyString()).submit()).thenReturn(
-        CompletableFuture.completedFuture(mock(Message.class)));
-
-    when(mockEvent.getName()).thenReturn("unknown");
-
-    when(mockEvent.reply(anyString())).thenReturn(mockReplyCallbackAction);
-    when(mockReplyCallbackAction.setEphemeral(true)).thenReturn(mockReplyCallbackAction);
-    when(mockReplyCallbackAction.submit());
-
-    CommandJoin commandJoin = new CommandJoin();
-    commandJoin.onSlashCommandInteraction(mockEvent);
-
-    verify(mockEvent).getName(); // ensure that getName() is called once
-  }
 
   @Test
   void notFromGuildTest() {

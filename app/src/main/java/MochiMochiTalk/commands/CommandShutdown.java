e9e192b991409e9b5c2b81a7bcd09411923cea19
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-12-12 19:56:09 +0900

    rewrite `shutdown` command

--- app/src/main/java/MochiMochiTalk/commands/CommandShutdown.java
+++ app/src/main/java/MochiMochiTalk/commands/CommandShutdown.java
@@ -1,29 +1,45 @@
 package MochiMochiTalk.commands;
 
+import MochiMochiTalk.api.CommandInformation;
 import MochiMochiTalk.util.DiscordServerOperatorUtil;
 import javax.annotation.Nonnull;
+import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 
-public class CommandShutdown extends ListenerAdapter {
-
-  // Logger
-  private static final Logger LOG = LoggerFactory.getLogger(CommandShutdown.class);
+@Slf4j
+public class CommandShutdown extends CommandInformation {
 
   @Override
-  public void onSlashCommandInteraction(@Nonnull SlashCommandInteractionEvent event) {
-    if (event.getName().equals("shutdown")) {
-      LOG.info("Shutdown has been requested by : {}", event.getUser());
-      if (!DiscordServerOperatorUtil.isMutsucordOperator(event.getUser().getId())) {
-        event.reply("このコマンドは管理者のみ使用できます。").setEphemeral(true).queue();
-        return;
-      }
-      LOG.info("confirmed identity. invoking shutdown");
-      event.reply("終了しています……おやすみなさい。プロデューサーさん").setEphemeral(true)
-          .queue(suc -> suc.getJDA().shutdown());
-    }
+  public String getCommandName() {
+    return "shutdown";
   }
 
+  @Override
+  protected String getCommandDescription() {
+    return "Botを強制的に停止させます。";
+  }
+
+  @Override
+  protected void setCommandData() {
+    if (this.commandData != null) {
+      return;
+    }
+    this.commandData = Commands.slash(
+      this.getCommandName(),
+      this.getCommandDescription()
+    ).setGuildOnly(true);
+  }
+
+  @Override
+  public void slashCommandHandler(@Nonnull SlashCommandInteractionEvent event) {
+    log.info("Shutdown has been requested by : {}", event.getUser());
+    if (!DiscordServerOperatorUtil.isMutsucordOperator(event.getUser().getId())) {
+      event.reply("このコマンドは管理者のみ使用できます。").setEphemeral(true).queue();
+      return;
+    }
+    log.info("confirmed identity. invoking shutdown");
+    event.reply("終了しています……おやすみなさい。プロデューサーさん").setEphemeral(true)
+      .queue(suc -> suc.getJDA().shutdown());
+  }
 }
